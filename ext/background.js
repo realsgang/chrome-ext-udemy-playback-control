ece3a7f5f2eba36cfadc93cac23e5f7ca10b520d
realsgang <realsgang@gmail.com>
2025-08-30 15:58:59 +0900

    Update background.js

--- ext/background.js
+++ ext/background.js
@@ -1,7 +1,8 @@
 chrome.commands.onCommand.addListener((command) => {
   chrome.tabs.query({ url: '*://*.udemy.com/*' }, (tabs) => {
     if (tabs.length > 0) {
-      chrome.tabs.sendMessage(tabs[0].id, { command: command });
+      chrome.tabs.sendMessage(tabs[0].id, { command })
+        .catch(err => console.warn("메시지 전송 실패:", err));
     }
   });
 });
